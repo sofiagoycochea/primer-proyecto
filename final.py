6e3eba8f840da6877db5992d5762d7c0df52952d
NATALIA TELLO <106551597+NATYTELLO33@users.noreply.github.com>
2022-08-10 20:59:48 -0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,11 @@
+# EJERCICIO 8 
+
+Historial4 = [7510, 7960, 76180, 800, 4100]
+
+def funcion(Historial):
+    resultado = min( Historial )
+    print('el valor mínimo de atención gastada en Olivia es:', resultado)
+
+
+funcion(Historial4)
+
