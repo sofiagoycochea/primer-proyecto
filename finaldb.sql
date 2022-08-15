c92a9c5d962fc973f5b0acb64ad5973730d00d51
Fernando Rosales <106622616+Fer-Rosales@users.noreply.github.com>
2022-08-14 23:43:20 -0300

    Ejercicio 10 SQL

---
+++ finaldb.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE peluqueriaCanina;
+	
+USE peluqueriaCanina;
+
+CREATE TABLE Dueno(
+    DNI INT NOT NULL,
+    Nombre VARCHAR(20) NOT NULL,
+    Apellido VARCHAR(20) NOT NULL,
+    Telefono INT,
+    Direccion VARCHAR(80),
+    PRIMARY KEY (DNI)
+);
+
+CREATE TABLE Perro(
+    ID_perro INT NOT NULL AUTO_INCREMENT,
+    Nombre VARCHAR(20) NOT NULL,
+    Fecha_nacimiento DATE,
+    Sexo VARCHAR(1), -- F O M 
+    DNI_dueno INT,
+    PRIMARY KEY (ID_perro),
+    FOREIGN KEY (DNI_dueno) REFERENCES Dueno(DNI) 
+);
+
+CREATE TABLE Historial(
+    ID_historial INT NOT NULL AUTO_INCREMENT,
+    Fecha DATE  NOT NULL,
+    Perro INT NOT NULL,
+    Descripcion VARCHAR(50),
+    Monto INT NOT NULL,
+    PRIMARY KEY (ID_historial),
+    FOREIGN KEY (Perro) REFERENCES Perro(ID_perro)
+);
+
+INSERT INTO Dueno(DNI, Nombre, Apellido, Telefono, Direccion) VALUES (35916098, 'Fernando', 'Rosales', 4792997, 'Balcarce 876');
+INSERT INTO Perro(Nombre, Fecha_nacimiento, Sexo, DNI_dueno) VALUES ('Titi', 2017/01/07,'M', 35916098);
+
+-- EJERCICIO 10
+
+TRUNCATE TABLE Historial;
